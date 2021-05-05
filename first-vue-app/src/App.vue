27247cdc0b4477c07c3e73699f2355e298462c41
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-05 11:47:10 +0300

    Removed FirstComponent from main component.

--- first-vue-app/src/App.vue
+++ first-vue-app/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <FirstComponent/>
     <HeaderComponent/>
   </div>
 </template>
 
 <script>
-import FirstComponent from './components/FirstComponent.vue'
 import HeaderComponent from './components/HeaderComponent.vue'
 
 
 export default {
   name: 'App',
   components: {
-    FirstComponent,
     HeaderComponent
   }
 }
@@ -26,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
