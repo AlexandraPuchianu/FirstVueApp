47bf11bc2e79606c2bd004dc15868ef38fff307b
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-05 11:47:42 +0300

    Added HeaderComponent body and style.

--- first-vue-app/src/components/HeaderComponent.vue
+++ first-vue-app/src/components/HeaderComponent.vue
@@ -1,5 +1,16 @@
 <template>
-    <h3>Header Component</h3>
+    <div class="header">
+        <div class="links">
+            <a id="gmailLink" href="https://www.google.com/intl/ro/gmail/about/#" target="_top">Gmail</a>
+            <a id="imaginiLink" href="https://www.google.ro/imghp?hl=ro&ogbl" target="_top">Imagini</a>
+        </div>
+        <div class="menu">
+            <a id="menuButton" role="button"><i class="fa fa-bars fa-lg"></i></a>
+        </div>
+        <div class="connectButton">
+            <a href="https://accounts.google.com/signin/v2/identifier?hl=ro&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin"><button class="button">Conectați-vă</button></a>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,4 +20,47 @@
 </script>
 
 <style>
+.header{
+    height: 2rem;
+    width: 100%;
+    position: relative;
+    float: right;
+    display: inline-flex;
+    align-items: baseline;
+    justify-content: flex-end;
+    padding: 1rem;
+    padding-right: 1rem;
+}
+.header a{
+    text-decoration: none;
+    margin-left: 0.8rem;
+    font-family: arial,sans-serif;
+    color: black;
+    font-size: 13px;
+}
+
+.links a :hover{
+    border-bottom: 1px solid black;
+}
+
+.menu a{
+    border-radius: 50%;
+    margin-left: 2rem;
+}
+
+.button {
+    background-color: #1a73e8;
+    border: none;
+    color: white;
+    margin-left: 0.7rem;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    cursor: pointer;
+    height: 2.3rem;
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    border-radius: 4px;
+}
 </style>
