18e18ee73525077c7898f8b8329c713bba755a92
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-05 16:06:28 +0300

    Added ButtonsComponent body and style.

---
+++ first-vue-app/src/components/ButtonsComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="buttons">
+    <button type="submit" class="searchButton">Căutare Google</button>
+    <button type="submit" class="feelLuckyButton">Mă simt norocos</button>
+ </div>
+</template>
+
+<script> 
+    export default {
+        name: 'ButtonsComponent'
+    }
+</script>
+
+<style>
+.buttons{
+    display: inline-flex;
+    width: 100%;
+    justify-content: center;
+    margin-top: 1.6rem;
+}
+  
+.buttons button{
+    border: none;
+    height: 2.3rem;
+    width: 8.2rem;
+    margin-inline: 0.5rem;
+    font-family: arial,sans-serif;
+    font-size: 14px;
+    color: #3c4043;
+    background-color: #f8f9fa;
+    border: 1px solid #f8f9fa;
+}
+</style>
