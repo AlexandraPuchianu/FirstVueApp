72ea681b0f499f59685f462e9544fbc4a1e87e19
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-05 16:15:01 +0300

    Added OtherLanguageComponent body and style.
    Changed text color for entire app.

--- first-vue-app/src/components/OtherLanguageComponent.vue
+++ first-vue-app/src/components/OtherLanguageComponent.vue
@@ -1,5 +1,11 @@
 <template>
-    <p>Other Language component!</p>
+    <div class="otherLanguage">
+        <p>Google oferit în: 
+            <a href="https://www.google.com/setprefs?sig=0_9RzuavNzVCjYRMlgzdaQhXKMZs8%3D&hl=hu&source=homepage&sa=X&ved=0ahUKEwiDtZKY5ZPwAhUiyYUKHXrOA-kQ2ZgBCA0">Magyar</a>
+            <a href="https://www.google.com/setprefs?sig=0_9RzuavNzVCjYRMlgzdaQhXKMZs8%3D&hl=de&source=homepage&sa=X&ved=0ahUKEwiDtZKY5ZPwAhUiyYUKHXrOA-kQ2ZgBCA4">Deutsch</a>
+            <a href="https://www.google.com/setprefs?sig=0_YEfyuLzJtrXlIMMoc-5fsK1jz6k%3D&hl=en&source=homepage&sa=X&ved=0ahUKEwiQoc3M5ZPwAhVRUhoKHTlABNIQ2ZgBCBA">English</a>
+        </p>
+    </div>
 </template>
 
 <script>
@@ -8,4 +14,18 @@
     }
 </script>
 
-<style></style>
+<style>
+.otherLanguage{
+    font-size: 14px;
+    font-family: arial,sans-serif;
+    display: inline-flex;
+    width: 100%;
+    justify-content: center;
+    margin-top: 1rem;
+}
+
+.otherLanguage a{
+    text-decoration: none;
+    margin-left: 0.4rem;
+}
+</style>
